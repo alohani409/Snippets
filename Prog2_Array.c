de83d905aa89ceb0b74b4428ed1ca38f8da96dd8
dgrchaitanya675 <93043325+dgrchaitanya675@users.noreply.github.com>
2021-10-23 19:30:45 +0530

    Update Prog2_Array.c

--- Prog2_Array.c
+++ Prog2_Array.c
@@ -1,45 +1,42 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
-intshowarr(int *arr, int *size)
+void showarr(int *arr, int *size)
 {
-    for (inti = 0; i < *size; i++)
+    for (int i = 0; i < *size; i++)
     {
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
-    return 0;
 }
 
-intaddelement(int *arr, int *size, intindex, intvalue)
+void addelement(int *arr, int *size, int index, int value)
 {
-    for (inti = *size; i > index; i--)
+    for (int i = *size; i > index; i--)
     {
         arr[i] = arr[i - 1];
     }
     arr[index] = value;
     *size += 1;
-    return 0;
 }
 
-intdelelement(int *arr, int *size, intindex)
-{
-    for (inti = index; i < *size - 1; i++)
+void delelement(int *arr, int *size, int index)
+{   
+    for (int i = index; i < *size - 1; i++)
     {
         arr[i] = arr[i + 1];
     }
     *size -= 1;
-    return 0;
 }
 
-intmain()
+void main()
 {
-    intsize;
-    intarr[20] = {1, 5, 7, 12};
+    int size;
+    int arr[20] = {1, 5, 7, 12};
     size = 4;
     showarr(arr, &size);
     addelement(arr, &size, 3, 6);
     showarr(arr, &size);
     delelement(arr, &size, 2);
     showarr(arr, &size);
-    return 0;
 }
